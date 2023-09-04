b47ca721db42e7e9ac8d40d710b06ba90a6837c7
JEXB <jippee.1211@gmail.com>
2023-09-04 18:46:24 +0700

    uncompleted

--- components/utils/store.ts
+++ components/utils/store.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { type User } from "./types";
+import { type User } from "./types-temp";
 interface Store {
   open: boolean;
   setOpen: (open: boolean) => void;
